1a4036c3119dca4dd962af85cd48f7d3e591a3da
Marc Schwyn <marcschwyn@Marcs-MacBook-Pro.local>
2025-11-12 15:17:29 +0700

    fix: handle undefined GA_MEASUREMENT_ID

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,7 +2,7 @@ export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-  if (typeof window !== 'undefined' && window.gtag) {
+  if (typeof window !== 'undefined' && window.gtag && GA_MEASUREMENT_ID) {
     window.gtag('config', GA_MEASUREMENT_ID, {
       page_path: url,
     })
